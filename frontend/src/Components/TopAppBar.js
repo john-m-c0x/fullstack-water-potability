402b19f4a9be040f226da892b294d0c8944b455d
Greg Williams <103441631@student.swin.edu.au>
2024-10-09 09:53:46 +1100

    Layout app bar

--- frontend/src/Components/TopAppBar.js
+++ frontend/src/Components/TopAppBar.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {AppBar, ToolBar, Typography, Button, Box} from '@mui/material';
+import {AppBar, Toolbar, Typography, Button, Box} from '@mui/material';
 
 export default function TopAppBar() {
     return (
         <AppBar position="static">
-            <Button>Test</Button>
+            <Toolbar>
+                <Box sx={{display:"flex", justifyContent:"space-between", alignItems:"center", width:"100%"}}>
+                    <Button variant="text" color="inherit">Page1</Button>
+                    <Box sx={{display:"flex", justifyContent:"center", alignItems:"center", gap:{xs:2, sm:10, md:20, lg:35}}}>
+                        <Button variant="text" color="inherit">Page2</Button>
+                        <Button variant="text" color="inherit">Page3</Button>
+                        <Button variant="text" color="inherit">Page4</Button>
+                    </Box>
+                    <Box sx={{ width: "50px" }}></Box>
+                </Box>
+            </Toolbar>
         </AppBar>
     );
 }
