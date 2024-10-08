e5fc21b2569d67179f9821e82c7e50da592f4793
Greg Williams <103441631@student.swin.edu.au>
2024-10-09 08:57:54 +1100

    Add app bar component

---
+++ frontend/src/Components/TopAppBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {AppBar, ToolBar, Typography, Button, Box} from '@mui/material';
+
+export default function TopAppBar() {
+    return (
+        <AppBar position="static">
+            
+        </AppBar>
+    );
+}
