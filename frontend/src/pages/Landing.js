eff193549b6a2cfa1f3cd0915925cb557d9f3e75
Greg Williams <103441631@student.swin.edu.au>
2024-10-14 09:18:27 +1100

    Layout landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Grid2, Typography, TextField } from '@mui/material';
 
 function Landing() {
   return (
-    <Box>
-        <Typography variant="h6" sx={{ marginLeft: 5 }}>
-            Landing Page Default Text
+    <Grid2 container spacing={12} direction="column" justifyContent="center" alignItems="center" sx={{height:"60vh"}}>
+      <Grid2 item xs={12} sx={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
+        <Typography variant="h5">
+          Transforming data into clarity.
         </Typography>
-    </Box>
+      </Grid2>
+      <Grid2 item xs={12} sx={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
+        <TextField variant="outlined" label="Prompt" sx={{width:{xs: '90%', sm: '70%', md: '50%', lg: '40%'}}}/>
+      </Grid2>
+    </Grid2>
   );
 }
 
