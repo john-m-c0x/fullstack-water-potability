b0486fb5077672c2d3fc5acd7bd0ae550b569ecf
Greg Williams <103441631@student.swin.edu.au>
2024-10-14 11:45:56 +1100

    Replace text field with prompt input field

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid2, Typography, TextField } from '@mui/material';
+import PromptInputField from '../components/PromptInputField';
 
 function Landing() {
   return (
@@ -9,8 +10,8 @@ function Landing() {
           Transforming data into clarity.
         </Typography>
       </Grid2>
-      <Grid2 item xs={12} sx={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
-        <TextField variant="outlined" label="Prompt" sx={{width:{xs: '90%', sm: '70%', md: '50%', lg: '40%'}}}/>
+      <Grid2 item xs={12} sx={{width:{xs: '90%', sm: '70%', md: '50%', lg: '40%'}}}>
+        <PromptInputField/>
       </Grid2>
     </Grid2>
   );
