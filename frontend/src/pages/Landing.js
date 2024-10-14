42dbbe1f97199d5c05613b7193c619e50064480c
Greg Williams <103441631@student.swin.edu.au>
2024-10-14 11:57:31 +1100

    Send prompt from landing to home page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import { Grid2, Typography, TextField } from '@mui/material';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Grid2, Typography } from '@mui/material';
 import PromptInputField from '../components/PromptInputField';
 
 function Landing() {
+  const naviagte = useNavigate();
+
+  //The prompt to send to home page, automatically updated via PrompInputFields 'onPromptChange'
+  const [prompt, setPrompt] = useState('');
+
+  //Send the prompt via state to home page when user taps enter - with key 'passedPrompt' and value 'prompt'
+  const passPromptToHome = (event) => {
+    if (event.key === "Enter") {
+      naviagte('/Home', {state: {passedPrompt: prompt}});
+    }
+  };
+
   return (
     <Grid2 container spacing={12} direction="column" justifyContent="center" alignItems="center" sx={{height:"60vh"}}>
       <Grid2 item xs={12} sx={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
@@ -11,7 +24,11 @@ function Landing() {
         </Typography>
       </Grid2>
       <Grid2 item xs={12} sx={{width:{xs: '90%', sm: '70%', md: '50%', lg: '40%'}}}>
-        <PromptInputField/>
+        <PromptInputField
+          prompt={prompt}
+          onKeyDown={passPromptToHome}
+          onPromptChange={(e) => setPrompt(e.target.value)}
+        />
       </Grid2>
     </Grid2>
   );
