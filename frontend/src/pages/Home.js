d9577faf2b973fe483463ae36854366854772630
Greg Williams <103441631@student.swin.edu.au>
2024-10-14 12:11:21 +1100

    Set home page input field based on state

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Grid2 } from '@mui/material';
+import PromptInputField from '../components/PromptInputField';
 
 function Home() {
+  const location = useLocation();
+
+  //Sets passedPrompt to value sent from landing page, or '' if sate is null
+  const {passedPrompt} = location.state || '';
+  const [prompt, setPrompt] = useState(passedPrompt || '')
+
   return (
-    <Box>
-        <Typography variant="h6" sx={{ marginLeft: 5 }}>
-            Home Page Default Text
-        </Typography>
-    </Box>
+    <Grid2 container spacing={2} sx={{ flexDirection:'column', height: '75vh', padding: 2, justifyContent: 'center', alignItems: 'center' }}>
+      <Grid2 item xs={12} sx={{width:{xs: '90%', sm: '70%', md: '60%', lg: '40%'}}}>
+        <PromptInputField
+          prompt={prompt}
+        />
+      </Grid2>
+    </Grid2>
   );
 }
 
